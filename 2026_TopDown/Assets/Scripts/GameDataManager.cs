5c33ead9582f464bd9cf4d5194f8c0f0c7c9a417
gdg0123 <gdg012377@gmail.com>
2026-05-21 15:30:53 +0900

    TopDown 플레이어 기본 세팅

---
+++ 2026_TopDown/Assets/Scripts/GameDataManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameDataManager : MonoBehaviour
+{
+    public static GameDataManager Instance;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
